bdeb4cde6d5b3caf8c2b7a4d353badfd73a1827b
gpetrousov <petrousov@gmail.com>
2026-05-27 17:29:27 +0300

    Fix ICES NQ Learner

--- pymarlzooplus/learners/__init__.py
+++ pymarlzooplus/learners/__init__.py
@@ -11,7 +11,7 @@ from .mat_learner import MATLearner
 from .maser_q_learner import  MASERQLearner
 from .maven_learner import MavenLearner
 from .commformer_learner import CommFormerLearner
-from .ices_learner  import ICESNQLearner
+from .ices_nq_learner  import ICESNQLearner
 
 REGISTRY = {
     "q_learner": QLearner,
@@ -27,5 +27,5 @@ REGISTRY = {
     "maser_q_learner": MASERQLearner,
     "maven_learner": MavenLearner,
     "commformer_learner": CommFormerLearner,
-    "ices_learner": CommFormerLearner,
+    "ices_nq_learner": ICESNQLearner,
 }
