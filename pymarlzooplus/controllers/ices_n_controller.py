a8fb965e094be5a810d60e4b48615130c55c358e
gpetrousov <petrousov@gmail.com>
2026-05-27 18:12:26 +0300

    Keep batch and agent dims isolated for ICES

--- pymarlzooplus/controllers/ices_n_controller.py
+++ pymarlzooplus/controllers/ices_n_controller.py
@@ -124,3 +124,28 @@ class ICESNMAC(NMAC):
 
         inputs = th.cat([x.reshape(bs, self.n_agents, -1) for x in inputs], dim=-1)
         return inputs
+
+    def _build_inputs(self, batch, t):
+        """
+        Override for ICES agent.
+        """
+
+        bs = batch.batch_size
+        inputs = []
+        
+        # 1. Append raw observations
+        inputs.append(batch["obs"][:, t])
+        
+        # 2. Append agent actions history
+        if self.args.obs_last_action:
+            if t == 0:
+                inputs.append(th.zeros_like(batch["actions_onehot"][:, t]))
+            else:
+                inputs.append(batch["actions_onehot"][:, t-1])
+                
+        # 3. Append Agent ID
+        if self.args.obs_agent_id:
+            inputs.append(th.eye(self.n_agents, device=batch.device).unsqueeze(0).expand(bs, -1, -1))
+
+        inputs = th.cat([x.reshape(bs, self.n_agents, -1) for x in inputs], dim=-1)
+        return inputs
