8703ce0719217feee0febef1a79718af21b61bc9
nikosgatos <gatos.nikolaos.01@gmail.com>
2026-07-13 19:32:20 +0300

    Revert MPE scenario loader for supported Python versions 3.8-3.11

--- pymarlzooplus/envs/multiagent_particle_envs/mpe/scenarios/__init__.py
+++ pymarlzooplus/envs/multiagent_particle_envs/mpe/scenarios/__init__.py
@@ -1,11 +1,7 @@
-import importlib.util
+import imp
 import os.path as osp
 
 
 def load(name):
     pathname = osp.join(osp.dirname(__file__), name)
-    module_name = f"{__name__}.{osp.splitext(name)[0]}"
-    spec = importlib.util.spec_from_file_location(module_name, pathname)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module
+    return imp.load_source('', pathname)
